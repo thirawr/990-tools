92dfad3ea42bdb767e83d8ec43cfeac60e256dd8
Tyler Davis <tyler.davis@chronicle.com>
2018-08-29 17:14:00 -0400

    field metadata list has basic view and template

--- tax_tools/core/views.py
+++ tax_tools/core/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views.generic import ListView, DetailView
-from core.models import Schedule_Part_Metadata, FilingFiling, Schedule_Metadata
+from core.models import Schedule_Part_Metadata, FilingFiling, Schedule_Metadata, Field_Metadata
 
 # Create your views here.
 class SkedPartList(ListView):
@@ -9,13 +9,14 @@ class SkedPartList(ListView):
     model = Schedule_Metadata
 
 
-class SkedPartDetail(ListView):
+class FieldsList(ListView):
     # this view will take a URL parameter indicating the sked part for which it should show fields
     # display variables in table format
-    pass
+    def get_queryset(self):
+        self.sked_part = get_object_or_404(Schedule_Part_Metadata, id=self.kwargs['sked_part_id'])
+        return Field_Metadata.objects.filter(parent_sked_part=self.sked_part)
 
-
-class VariableDetail(DetailView):
+class FieldDetail(DetailView):
     # show varaible metadata
     # optional?
     # maybe show some extra fields not on the VariableList table
