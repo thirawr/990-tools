75b2f5762226177c6b8a768628c95735a1a312ee
Tyler Davis <tyler.davis@chronicle.com>
2019-03-05 12:23:01 -0500

    Model tweaks

--- tax_tools/core/models.py
+++ tax_tools/core/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 
-
 # lets keep the core 990-centric models here
 '''MANAGED METADATA MODELS'''
 class Efile_Metadata(models.Model):
@@ -67,6 +66,14 @@ class Fiscal_Year(models.Model):
     fiscal_year = models.IntegerField(db_index=True, null=False)
     organization = models.ForeignKey(Organization, on_delete=models.CASCADE)
 
+
+# This is an auto-generated Django model module.
+# You'll have to do the following manually to clean this up:
+#   * Rearrange models' order
+#   * Make sure each model has one field with primary_key=True
+#   * Make sure each ForeignKey has `on_delete` set to the desired behavior.
+#   * Remove `managed = False` lines if you wish to allow Django to create, modify, and delete the table
+# Feel free to rename the models, but don't rename db_table values or field names.
 '''
 UNMANAGED 990 XML DATABASE MODELS
 NOTE: THESE _NEED_ TO BE UPDATED CONCURRENTLY WITH ANY CHANGES TO THE MODELS IN THE DEPLOYED 990-XML-DATABASE AND DB NAMES IN 990-XML-METADATA
@@ -74,17 +81,127 @@ https://github.com/jsfenfen/990-xml-metadata
 '''
 
 
+class AuthGroup(models.Model):
+    name = models.CharField(unique=True, max_length=80)
+
+    class Meta:
+        managed = False
+        db_table = 'auth_group'
+
+
+class AuthGroupPermissions(models.Model):
+    group = models.ForeignKey(AuthGroup, models.DO_NOTHING)
+    permission = models.ForeignKey('AuthPermission', models.DO_NOTHING)
+
+    class Meta:
+        managed = False
+        db_table = 'auth_group_permissions'
+        unique_together = (('group', 'permission'),)
+
+
+class AuthPermission(models.Model):
+    name = models.CharField(max_length=255)
+    content_type = models.ForeignKey('DjangoContentType', models.DO_NOTHING)
+    codename = models.CharField(max_length=100)
+
+    class Meta:
+        managed = False
+        db_table = 'auth_permission'
+        unique_together = (('content_type', 'codename'),)
+
+
+class AuthUser(models.Model):
+    password = models.CharField(max_length=128)
+    last_login = models.DateTimeField(blank=True, null=True)
+    is_superuser = models.IntegerField()
+    username = models.CharField(unique=True, max_length=150)
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=150)
+    email = models.CharField(max_length=254)
+    is_staff = models.IntegerField()
+    is_active = models.IntegerField()
+    date_joined = models.DateTimeField()
+
+    class Meta:
+        managed = False
+        db_table = 'auth_user'
+
+
+class AuthUserGroups(models.Model):
+    user = models.ForeignKey(AuthUser, models.DO_NOTHING)
+    group = models.ForeignKey(AuthGroup, models.DO_NOTHING)
+
+    class Meta:
+        managed = False
+        db_table = 'auth_user_groups'
+        unique_together = (('user', 'group'),)
+
+
+class AuthUserUserPermissions(models.Model):
+    user = models.ForeignKey(AuthUser, models.DO_NOTHING)
+    permission = models.ForeignKey(AuthPermission, models.DO_NOTHING)
+
+    class Meta:
+        managed = False
+        db_table = 'auth_user_user_permissions'
+        unique_together = (('user', 'permission'),)
+
+
+class DjangoAdminLog(models.Model):
+    action_time = models.DateTimeField()
+    object_id = models.TextField(blank=True, null=True)
+    object_repr = models.CharField(max_length=200)
+    action_flag = models.PositiveSmallIntegerField()
+    change_message = models.TextField()
+    content_type = models.ForeignKey('DjangoContentType', models.DO_NOTHING, blank=True, null=True)
+    user = models.ForeignKey(AuthUser, models.DO_NOTHING)
+
+    class Meta:
+        managed = False
+        db_table = 'django_admin_log'
+
+
+class DjangoContentType(models.Model):
+    app_label = models.CharField(max_length=100)
+    model = models.CharField(max_length=100)
+
+    class Meta:
+        managed = False
+        db_table = 'django_content_type'
+        unique_together = (('app_label', 'model'),)
+
+
+class DjangoMigrations(models.Model):
+    app = models.CharField(max_length=255)
+    name = models.CharField(max_length=255)
+    applied = models.DateTimeField()
+
+    class Meta:
+        managed = False
+        db_table = 'django_migrations'
+
+
+class DjangoSession(models.Model):
+    session_key = models.CharField(primary_key=True, max_length=40)
+    session_data = models.TextField()
+    expire_date = models.DateTimeField()
+
+    class Meta:
+        managed = False
+        db_table = 'django_session'
+
+
 class FilingFiling(models.Model):
-    submission_year = models.IntegerField(blank=True, null=True)
-    return_id = models.CharField(max_length=31, blank=True, null=True)
-    filing_type = models.CharField(max_length=31, blank=True, null=True)
-    ein = models.CharField(max_length=31)
-    tax_period = models.IntegerField(blank=True, null=True)
-    sub_date = models.CharField(max_length=31, blank=True, null=True)
-    taxpayer_name = models.CharField(max_length=255, blank=True, null=True)
-    return_type = models.CharField(max_length=7, blank=True, null=True)
-    dln = models.CharField(max_length=31, blank=True, null=True)
-    object_id = models.CharField(max_length=31, blank=True, null=True)
+    submission_year = models.IntegerField()
+    return_id = models.CharField(max_length=8)
+    filing_type = models.CharField(max_length=5)
+    ein = models.CharField(max_length=9)
+    tax_period = models.IntegerField()
+    sub_date = models.CharField(max_length=22)
+    taxpayer_name = models.CharField(max_length=100)
+    return_type = models.CharField(max_length=5)
+    dln = models.CharField(max_length=14)
+    object_id = models.CharField(max_length=18)
     schema_version = models.TextField(blank=True, null=True)
     tax_year = models.IntegerField(blank=True, null=True)
     parse_started = models.IntegerField(blank=True, null=True)
@@ -94,13 +211,9 @@ class FilingFiling(models.Model):
     key_error_count = models.IntegerField(blank=True, null=True)
     error_details = models.TextField(blank=True, null=True)
 
-    def __str__(self):
-        return self.taxpayer_name
-
     class Meta:
         managed = False
         db_table = 'filing_filing'
-
 
 class ReturnCntrctrcmpnstn(models.Model):
     object_id = models.CharField(max_length=31, blank=True, null=True)
@@ -3799,6 +3912,8 @@ class ReturnSkedaPartV(models.Model):
     dstrbtnallctns_excssfrmyr3amt = models.BigIntegerField(db_column='DstrbtnAllctns_ExcssFrmYr3Amt', blank=True, null=True)  # Field name made lowercase.
     dstrbtnallctns_excssfrmyr2amt = models.BigIntegerField(db_column='DstrbtnAllctns_ExcssFrmYr2Amt', blank=True, null=True)  # Field name made lowercase.
     dstrbtnallctns_excssfrmyr1amt = models.BigIntegerField(db_column='DstrbtnAllctns_ExcssFrmYr1Amt', blank=True, null=True)  # Field name made lowercase.
+    excssdstrbtncyvyr4amt = models.BigIntegerField(db_column='ExcssDstrbtnCyvYr4Amt', blank=True, null=True)  # Field name made lowercase.
+    excssfrmyr5amt = models.BigIntegerField(db_column='ExcssFrmYr5Amt', blank=True, null=True)  # Field name made lowercase.
 
     class Meta:
         managed = False
