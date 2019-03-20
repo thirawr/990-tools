f3ccecf8dd593fef7516b992429133aea292cc3f
Tyler Davis <tyler.davis@chronicle.com>
2019-03-20 15:24:39 -0400

    Converted private credentials to env vars

--- tax_tools/tax_tools/private.py
+++ tax_tools/tax_tools/private.py
@@ -1,19 +1,12 @@
+import os
+import dj_database_url
+
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = None
+SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 DATABASES = {
-    'default': {
-        'NAME': '',
-        'ENGINE': 'django.db.backends.mysql',
-        'USER': '',
-        'PASSWORD': ''
-    },
-    '990s': {
-        'NAME': '',
-        'ENGINE': 'django.db.backends.mysql',
-        'USER': '',
-        'PASSWORD': ''
-    }
+    'default': dj_database_url.parse(os.getenv('TAX_TOOLS_DB_URL')),
+    '990s': dj_database_url.parse(os.getenv('TAX_RETURN_DB_URL'))
 }
