ff5d6a1434da620c04ac5bc7673af920d4309a3e
Tyler Davis <tyler.davis@chronicle.com>
2019-04-03 14:41:21 -0400

    Configured prod file for debug=False

--- tax_tools/settings/prod.py
+++ tax_tools/settings/prod.py
@@ -1,7 +1,8 @@
 from tax_tools.settings.common import *
 from tax_tools.settings.private import *
 
-DEBUG = True
-TEMPLATE_DEBUG = True
-ALLOWED_HOSTS = ['.herokuapp.com', '0.0.0.0']
+DEBUG = False
+TEMPLATE_DEBUG = False
+ALLOWED_HOSTS = ['herokuapp.com', '0.0.0.0']
+# print(ALLOWED_HOSTS)
 INSTALLED_APPS += ()
